34f178f2c0aa484e1bdb5f2c5782155f07c220ae
McodeQ <1127964507@qq.com>
2017-08-14 09:50:11 +0800

    算法练习

---
+++ src/exercise/E1_2_3.java
@@ -0,0 +1,12 @@
+package exercise;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class E1_2_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StdOut.print("agag");
+	}
+
+}
